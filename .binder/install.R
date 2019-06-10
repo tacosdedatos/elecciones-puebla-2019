911a5a21a91d2a74a30dadc07415f6dcd026c70b
Sergio Sánchez Zavala <chekos.data@gmail.com>
2019-06-10 15:36:42 -0700

    Update install.R

--- .binder/install.R
+++ .binder/install.R
@@ -1,1 +1,1 @@
-install.packages(c("plyr", "dplyr", "tidyr", "ggplot2", "readxl",, "tidyverse", "stringr", "raster", "viridis"))
+install.packages(c("plyr", "dplyr", "tidyr", "ggplot2", "readxl", "tidyverse", "stringr", "raster", "viridis"))
